a82c70ff80ca91f29ea115cd1b96185b46c14bbf
Justin Hong <jhong@zmacl027.local>
2020-03-10 12:02:10 -0700

    set up controllers

---
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConstructorInjectedController {
+    private final GreetingService greetingService; // can not be modified after creation
+
+    // constructor is injecting the greetingService dependency
+    // @Autowired is not required for constructors
+    public ConstructorInjectedController(@Qualifier("constructorInjectedGreetingService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+
+}
